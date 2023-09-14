a7e1bc5e7dfea21feea6519f0fac87e496ebafe0
webdevtrev <66264441+webdevtrev@users.noreply.github.com>
2023-09-14 19:47:56 -0400

    Adds meta tag to prevent iphone zoom in

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 import { Inter } from 'next/font/google';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -12,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
+      <Head>
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+      </Head>
       <body className={inter.className}>
         <main>{children}</main>
       </body>
